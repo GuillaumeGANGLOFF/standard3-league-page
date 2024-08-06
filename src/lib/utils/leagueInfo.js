27e3a4cc32af18aedb1362122f80bc4c383e4cd7
GuillaumeGANGLOFF <148479543+GuillaumeGANGLOFF@users.noreply.github.com>
2024-08-06 10:47:48 +0200

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "998016479058726912"; // your league ID
+export const leagueID = "1123588729257431040"; // your league ID
 export const leagueName = "FantasyBallers Standard 3"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
